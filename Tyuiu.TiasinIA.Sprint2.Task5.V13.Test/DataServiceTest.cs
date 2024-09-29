80fb82cd7656c570ad7ec35c8d51628e13f2db20
Илья Тясин <turbopad7200@gmail.com>
2024-09-29 16:18:48 +0500

    Task5

---
+++ Tyuiu.TiasinIA.Sprint2.Task5.V13.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.TiasinIA.Sprint2.Task5.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()//
+        {
+        }
+    }
+}
