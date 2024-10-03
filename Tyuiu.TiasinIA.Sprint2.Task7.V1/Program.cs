5c75afc002f9c45daf4f5e0bfe37e8c61228948a
Илья Тясин <turbopad7200@gmail.com>
2024-10-03 13:24:26 +0500

    Task7

--- Tyuiu.TiasinIA.Sprint2.Task7.V1/Program.cs
+++ Tyuiu.TiasinIA.Sprint2.Task7.V1/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Tyuiu.TiasinIA.Sprint2.Task7.V1
+﻿using Tyuiu.TiasinIA.Sprint2.Task7.V1.Lib;
+namespace Tyuiu.TiasinIA.Sprint2.Task7.V1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Тясин И. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Тясин Илья Александрович | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные           *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка с координатами  *");
+            Console.WriteLine("* X,Y в заштрихованной области.\r\n\r\n                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение у:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            
+
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.CheckDotInShadedArea(x,y));
+            Console.ReadKey();
         }
     }
 }
